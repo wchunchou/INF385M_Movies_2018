bbb63564d2e77a3c2729b42f0573a53a8776ede0
weichun <kickdesk@gmail.com>
2018-11-27 19:10:00 -0600

    create files

---
+++ db_connection.php
@@ -0,0 +1,5 @@
+$host = "localhost";
+$username = "wc9727";
+$password = "12345678";
+$database = "Movie";
+$link = mysqli_connect($host, $username, $password, $database);
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    include_once("header.html");
+    include("db_connection.php");
+
+
+    include_once("footer.html");
+?>
