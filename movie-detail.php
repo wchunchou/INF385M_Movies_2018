e26e21ddca9662b26f4c88fa084553c70b34f3e2
weichun <kickdesk@gmail.com>
2018-12-08 17:23:20 -0600

    edit

--- movie-detail.php
+++ movie-detail.php
@@ -1,8 +1,15 @@
 <?php
     include_once("header.html");
     include("db_connection.php");
-    $results = mysqli_query($link, "SQL SYNTAX HERE");
-    $rows = mysqli_fetch_array($results);
+    $id=$_GET["id"];
+    $movie_sql= "SELECT Movie.movie_name, Movie.imdb_id, Movie.year_released, Movie.run_time, Movie.image, Movie.story_line, Genre.genre, Directors.director_name, Awards.award_name FROM Movie JOIN Genre ON Movie.genre_id= Genre.genre_id JOIN Directors ON Movie.director_id= Directors. director_id  JOIN Awards ON Movie.award_id= Awards.award_id Where Movie.movie_id= 6";
+    $actor_sql ="Select Actors.actor_name from Actors Join MovieActorJunction On Actors.actor_id= MovieActorJunction.actor_id Join Movie On MovieActorJunction.movie_id= Movie.movie_id Where Movie.movie_id=6";
+    $tag_sql="Select MovieTags.tag_name from MovieTags Join MovieTagJunction On MovieTags.tag_id= MovieTagJunction.tag_id Join Movie On MovieTagJunction.movie_id= Movie.movie_id Where Movie.movie_id=6";
+    $results = mysqli_query($link, $movie_sql);
+    while ($rows = mysqli_fetch_array($results)) {    
+        print "$rows[table_column here]"; 
+    };
+
     while ($rows = mysqli_fetch_array($results)) {    
         print "$rows[table_column here]"; 
     };
--- tag.php
+++ tag.php
@@ -1,22 +1,44 @@
 <?php
     include_once("header.php");
     include("db_connection.php");
-$id= $_GET["id"];
-//print $id;
-$tags = "SELECT Movie.movie_name FROM MovieTagJunction JOIN MovieTags ON MovieTags.tag_id=MovieTagJunction.tag_id JOIN Movie ON MovieTagJunction.movie_id=Movie.movie_id WHERE MovieTags.tag_id=".$id; 
-
-
+    $id= $_GET["id"];
+    //print $id;
+    $current_tag = "SELECT tag_name FROM  MovieTags WHERE tag_id=".$id;
+    $tags = "SELECT * FROM MovieTagJunction JOIN MovieTags ON MovieTags.tag_id=MovieTagJunction.tag_id JOIN Movie ON MovieTagJunction.movie_id=Movie.movie_id WHERE MovieTags.tag_id=".$id; 
     $results = mysqli_query($link, $tags);
+    $result_current_tag = mysqli_query($link, $current_tag);
     
+?>
 
-    while ($rows = mysqli_fetch_array($results)) {    
-        print "$rows[movie_name] <br/>"; 
-    };
-
-
-
-
-
+    <div class="container mt-xlg movielist">
+        <div class="row">
+            <div class="col-md-8">
+            <h2>Tag: <?php while ($rows = mysqli_fetch_array($result_current_tag)) {
+                print $rows["tag_name"];
+            } ?></h2>
+            <?php
+                while ($rows = mysqli_fetch_array($results)) {     
+            ?>
+                <div class="card">
+                    <div class="row no-gutters">
+                        <div class="col-auto">
+                            <a href="movie-detail.php?id=<?php print $rows["movie_id"]?>"><img src=" <?php print $img_path.$rows["image"]; ?>" class="img-fluid" alt="" width="200px"></a>
+                        </div>
+                        <div class="col">
+                            <div class="card-block p-2">
+                                <h4 class="card-title"><a href="movie-detail.php?id=<?php print $rows["movie_id"]?>"><?php print $rows["movie_name"];?></a></h4>
+                                <p class="card-text"><?php print $rows["year_released"]." | ".$rows["run_time"]." mins | ".$rows["genre"];?></p>
+                                <p class="card-text"><?php print $rows["story_line"];?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php }; ?>
+            </div>
+        </div>
+    </div>  
+<?php
     mysqli_close($link);
     include_once("footer.php");
 ?>
+
