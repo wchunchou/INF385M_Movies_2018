54dd4572cb26fe5b755d33bbd0bb87ebf471ff5c
weichun <kickdesk@gmail.com>
2018-12-08 17:33:47 -0600

    upload sam's movie_detail

--- movie-detail.php
+++ movie-detail.php
@@ -1,21 +1,28 @@
 <?php
-    include_once("header.html");
+    include_once("header.php");
     include("db_connection.php");
-    $id=$_GET["id"];
-    $movie_sql= "SELECT Movie.movie_name, Movie.imdb_id, Movie.year_released, Movie.run_time, Movie.image, Movie.story_line, Genre.genre, Directors.director_name, Awards.award_name FROM Movie JOIN Genre ON Movie.genre_id= Genre.genre_id JOIN Directors ON Movie.director_id= Directors. director_id  JOIN Awards ON Movie.award_id= Awards.award_id Where Movie.movie_id= 6";
-    $actor_sql ="Select Actors.actor_name from Actors Join MovieActorJunction On Actors.actor_id= MovieActorJunction.actor_id Join Movie On MovieActorJunction.movie_id= Movie.movie_id Where Movie.movie_id=6";
-    $tag_sql="Select MovieTags.tag_name from MovieTags Join MovieTagJunction On MovieTags.tag_id= MovieTagJunction.tag_id Join Movie On MovieTagJunction.movie_id= Movie.movie_id Where Movie.movie_id=6";
-    $results = mysqli_query($link, $movie_sql);
-    while ($rows = mysqli_fetch_array($results)) {    
-        print "$rows[table_column here]"; 
+$id= $_GET["id"];
+    $moviedetail= "SELECT Movie.movie_name, Movie.imdb_id, Movie.year_released, Movie.run_time, Movie.image, Movie.story_line, Genre.genre, Directors.director_name, Awards.award_name FROM Movie JOIN Genre ON Movie.genre_id= Genre.genre_id JOIN Directors ON Movie.director_id= Directors. director_id  JOIN Awards ON Movie.award_id= Awards.award_id Where Movie.movie_id=".$id;
+    $actor= "Select Actors.actor_name from Actors Join MovieActorJunction On Actors.actor_id= MovieActorJunction.actor_id Join Movie On MovieActorJunction.movie_id= Movie.movie_id Where Movie.movie_id=".$id;
+    $tag= "Select MovieTags.tag_name from MovieTags Join MovieTagJunction On MovieTags.tag_id= MovieTagJunction.tag_id Join Movie On MovieTagJunction.movie_id= Movie.movie_id Where Movie.movie_id=".$id;
+    $results = mysqli_query($link, $moviedetail);
+
+    while ($rows = mysqli_fetch_array($results)) {
+        print "$rows[movie_name]<br/>";
+	print "$rows[imdb_id]<br/>";
+	print "$rows[year_released]<br/>";
+	print "$rows[run_time]<br/>";
+	print "$rows[image]<br/>";
+	print "$rows[story_line]<br/>"; 
     };
-
-    while ($rows = mysqli_fetch_array($results)) {    
-        print "$rows[table_column here]"; 
-    };
-
-
-
+ 	$solution= mysqli_query($link, $actor);
+	while ($columns = mysqli_fetch_array($solution)) {
+	print "$columns[actor_name]<br/>";
+};
+	$length= mysqli_query($link, $tag);
+	while ($answer = mysqli_fetch_array($length)) {
+	print "$answer[tag_name]<br/>";
+};
     mysqli_close($link);
-    include_once("footer.html");
+    include_once("footer.php");
 ?>
